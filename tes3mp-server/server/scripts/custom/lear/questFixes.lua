694df4f02b136a3d5b2d33b07bc919e5b932e2cb
Matt <ddrlear@yahoo.com>
2019-12-09 18:44:29 -0600

    updated delete method
    Updated method of deleting spawn, to prevent kicking players for use of console.

--- tes3mp-server/server/scripts/custom/lear/questFixes.lua
+++ tes3mp-server/server/scripts/custom/lear/questFixes.lua
@@ -37,19 +37,17 @@ customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid
 	-- If player enters nearby cells of staada's location:
 	if currentCellDescription == "3, 23" or currentCellDescription == "2, 24" or currentCellDescription == "2, 23" or currentCellDescription == "3, 24" then
 		-- If player has the appropriate quest index only:
-		if tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "da_azura", index = 10 }, true) and not tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "da_azura", index = 20 }, true) then
+		if hasJournalIndex(pid, "da_azura", 10) and not hasJournalIndex(pid, "da_azura", 20) then
 			
 			local targetRefId = "golden saint_staada" -- creature or npc id.
-			local respawnMinutes = 15 -- respawn time in minutes.
-			local respawnHours = 0 -- respawn time in hours
-			local respawnDays = 0 -- respawn time in days
+			local respawnTime = 900 -- respawn time in seconds.
 			local spawnOrPlace = "spawn" -- "spawn" for creatures or "place" for NPCs
 			local refIdVariable = "golden_saint_staada" --  This is what is saved within the world file for the respawn timer
 			
 			local destination = "3, 24" -- Cell to spawn creature/NPC in.
 			local location = {posX = 29314.740234, posY = 197027.375000, posZ = 352.015381, rotX = 0, rotY = 0, rotZ = 0} -- Position to spawn creature/NPC in.
 		
-			local serverTime = tonumber(os.date("%H.%M"))--tonumber(os.date("%H.%M"))
+			local serverTime = os.time() --tonumber(os.date("%H.%M"))--tonumber(os.date("%H.%M"))
 			local creatOrNpc = "creatureRespawns" -- No need to touch.
 			if spawnOrPlace == "spawn" then
 				creatOrNpc = "creatureRespawns"
@@ -57,50 +55,35 @@ customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid
 				creatOrNpc = "npcRespawns"
 			end
 			
-			logicHandler.RunConsoleCommandOnPlayer(pid, "\"" .. targetRefId .. "\"->setdelete 1", forEveryone)
-			Methods.questMobSpawn(pid, creatOrNpc, refIdVariable, respawnMinutes, respawnHours, respawnDays, serverTime, destination, location)
+			for slot, uIndex in pairs(LoadedCells[currentCellDescription].data.objectData) do
+				if uIndex.refId == refIdVariable then
+					logicHandler.DeleteObjectForPlayer(pid, currentCellDescription, uIndex)
+				end
+			end
 			
+			if WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] == nil or serverTime >= WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] then 
+				for slot, uIndex in pairs(LoadedCells[currentCellDescription].data.objectData) do
+					if uIndex.refId == refIdVariable then
+						logicHandler.DeleteObjectForPlayer(pid, currentCellDescription, uIndex)
+						Methods.questMobSpawn(pid, creatOrNpc, refIdVariable, respawnTime, serverTime, destination, location)
+					end
+				end
+			end
+
 		end
 	end
 
 end)
 
-Methods.questMobSpawn = function(pid, creatOrNpc, refIdVariable, respawnMinutes, respawnHours, respawnDays, serverTime, destination, location)
+Methods.questMobSpawn = function(pid, creatOrNpc, refIdVariable, respawnTime, serverTime, destination, location)
 	
-	if WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] == nil or serverTime >= WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] then
-			
+	if WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] == nil or serverTime >= WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] then 
+		
 		logicHandler.CreateObjectAtLocation(destination, location, targetRefId, spawnOrPlace)
 		
-		local actualMinute = os.date("%M")
-		local actualHour = os.date("%H")
+		local setRespawnTime = os.time() + respawnTime
 		
-		while respawnMinutes > 59 do
-			respawnMinutes = respawnMinutes - 60
-			respawnHours = respawnHours + 1
-		end
-		
-		while respawnHours > 23 do
-			respawnHours = respawnHours - 24
-			respawnDays = respawnDays + 1
-		end
-		
-		actualMinute = actualMinute + respawnMinutes
-		
-		if actualMinute > 59 then
-			actualMinute = actualMinute - 60
-			rollHour = respawnHours + 1
-		end
-		
-		actualHour = actualHour + respawnHours
-		
-		if actualHour > 23 then
-			actualHour = actualHours - 24
-			respawnDays = respawnDays + 1
-		end
-		
-		local finalTime = actualHour .. actualMinute
-		
-		WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] = tonumber(finalTime)
+		WorldInstance.data.customVariables.respawning[creatOrNpc][refIdVariable] = setRespawnTime
 		WorldInstance:Save()
 	end
 end
