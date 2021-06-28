ca45f90b628eb4239a65be9827369d3c4d850035
Matt <ddrlear@yahoo.com>
2021-06-28 11:47:24 -0500

    Updated for simplicity.
    Updated for simplicity.

--- tes3mp-server/server/scripts/custom/lear/prematureCorpusFix.lua
+++ tes3mp-server/server/scripts/custom/lear/prematureCorpusFix.lua
@@ -1,6 +1,6 @@
 --[[
 Dagoth Gares Premature Corprus Fix
-	version 1.01
+	version 1.03
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 This script will prevent Dagoth Gares from giving players Corprus unless they are on the correct quest to do so.
@@ -8,11 +8,10 @@ And for the player to progress when they do have the correct quest, they must ac
 This will prevent accidental and pre-mature Corprus acquisition.
 
 INSTALLATION:
-This lua script should be placed in your tes3mp-servers server/scripts/custom/lear folder. If you do not have a lear folder,
-simply add one and place this script within.
+This lua script should be placed in your tes3mp-servers server/scripts/custom folder.
 
 Then, in the server/scripts folder, add the following line of text to the bottom of this page:
-require("custom.lear.prematureCorpusFix")
+require("custom.prematureCorpusFix")
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 DO NOT EDIT BEYOND THIS, UNLESS YOU KNOW WHAT YOU'RE DOING.
@@ -34,13 +33,20 @@ customEventHooks.registerHandler("OnServerPostInit", function(eventStatus)
 	updateDagothGaresRecord()
 end)
 
+local addCorprusItem = function(pid, refId)
+	if refId == nil then return end
+	tes3mp.ClearInventoryChanges(pid)
+	tes3mp.SetInventoryChangesAction(pid, enumerations.inventory.ADD)
+	tes3mp.AddItemChange(pid, refId, 1, -1, -1, "")
+	tes3mp.SendInventoryChanges(pid)
+	Players[pid]:SaveInventory()
+end
 
-local split = function(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
+local hasCorprusQuest = function(pid, qId, qIndex)
+	if tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = qId, index = qIndex }, true) then
+		return true
+	end
+	return false
 end
 
 customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid, cellDescription, objects, players)
@@ -50,10 +56,6 @@ customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid
 	local isValid = eventStatus.validDefaultHandler
 
     for n,object in pairs(objects) do
-		
-        local temp = split(object["uniqueIndex"], "-")
-        local RefNum = temp[1]
-        local MpNum = temp[2]
 		
 		local objectUniqueIndex = object.uniqueIndex
 		local objectRefId = object.refId
@@ -69,21 +71,21 @@ customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid
 			-- if dead, check if player has appropriate journal entry.
 			else 
 				isValid = false
-				if tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "a2_2_6thhouse", index = 5 }, true) and not tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "a2_2_6thhouse", index = 50 }, true) and not tableHelper.containsValue(Players[pid].data.customVariables.lear.questFixes.dagothGares, "garesCorprusObtained") then
+				if hasCorprusQuest(pid, "a2_2_6thhouse", 5) and not hasCorprusQuest(pid, "a2_2_6thhouse", 50) and not tableHelper.containsValue(Players[pid].data.customVariables.dagothGares, "garesCorprusObtained") then
 					local dagothGaresDeathMessage =  "With his dying breath, Dagoth Gares smiles and curses you. 'Even as my Master wills, you shall come to him, in his flesh, and of his flesh."
 					tes3mp.CustomMessageBox(pid, -1, dagothGaresDeathMessage,"Ok")
 					logicHandler.RunConsoleCommandOnPlayer(pid, "journal a2_2_6thhouse 50")
 					logicHandler.RunConsoleCommandOnPlayer(pid, "player->AddSpell \"Corprus\"")
 					
-					logicHandler.RunConsoleCommandOnPlayer(pid, "player->additem \"bk_a2_2_dagoth_message\" 1")
+					addCorprusItem(pid, "bk_a2_2_dagoth_message")
 					logicHandler.RunConsoleCommandOnPlayer(pid, "player->playsound \"Item Misc Up\"")
 					tes3mp.MessageBox(pid, -1, "Message from Dagoth Ur has been added to your inventory.")
 					
-					logicHandler.RunConsoleCommandOnPlayer(pid, "player->additem \"amulet of 6th house\" 1")
+					addCorprusItem(pid, "amulet of 6th house")
 					logicHandler.RunConsoleCommandOnPlayer(pid, "player->playsound \"Item Clothes Up\"")
 					tes3mp.MessageBox(pid, -1, "6th House Amulet has been added to your inventory.")
 					
-					table.insert(Players[pid].data.customVariables.lear.questFixes.dagothGares, "garesCorprusObtained")
+					table.insert(Players[pid].data.customVariables.dagothGares, "garesCorprusObtained")
 				end
 			end
 		end
@@ -94,17 +96,16 @@ customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid
 end)
 
 customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid, previousCellDescription, currentCellDescription)
--- 
 	if currentCellDescription == "Ilunibi, Soul's Rattle" then
 		-- Set up the bugfix table for this cell.
-		if Players[pid].data.customVariables.lear.questFixes.dagothGares == nil then
-			Players[pid].data.customVariables.lear.questFixes.dagothGares = {}
+		if Players[pid].data.customVariables.dagothGares == nil then
+			Players[pid].data.customVariables.dagothGares = {}
 		end
 		
 		-- forcedGreeting bit
-		if tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "a2_2_6thhouse", index = 5 }, true) and not tableHelper.containsKeyValuePairs(Players[pid].data.journal, { quest = "a2_2_6thhouse", index = 50 }, true) and not tableHelper.containsValue(Players[pid].data.customVariables.lear.questFixes.dagothGares, "forcedGreeting") then
+		if hasCorprusQuest(pid, "a2_2_6thhouse", 5)  and not hasCorprusQuest(pid, "a2_2_6thhouse", 50) and not tableHelper.containsValue(Players[pid].data.customVariables.dagothGares, "forcedGreeting") then
 			logicHandler.RunConsoleCommandOnPlayer(pid, "\"dagoth gares\"->forcegreeting")
-			table.insert(Players[pid].data.customVariables.lear.questFixes.dagothGares, "forcedGreeting")
+			table.insert(Players[pid].data.customVariables.dagothGares, "forcedGreeting")
 		end	
 	end
 end)
